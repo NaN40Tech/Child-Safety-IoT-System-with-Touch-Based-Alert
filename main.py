d851489f139d2ec28096cfdfe20551520c2a54e2
Nabila Salvaningtyas <49722727+NaN40Tech@users.noreply.github.com>
2025-07-15 18:53:34 +0700

    update main

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from flask import Flask, request
-import requests
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "ESP32 Gateway Aktif"
-
-@app.route("/webhook", methods=["GET"])
-def webhook():
-    msg = request.args.get("msg", "pesan kosong")
-    send_telegram(msg)
-    return f"Pesan dikirim: {msg}"
-
-def send_telegram(msg):
-    bot_token = "8175344760:AAFvyssP6vnamA5lqhwGSxsGzhmPuvNRzoM"
-    chat_id = "1712293814"
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    requests.post(url, data={"chat_id": chat_id, "text": msg})
+@app.route('/', methods=['GET', 'POST'])
+def handle_message():
+    # Ambil parameter dari SIM800L (misalnya dari URL: ?msg=Halo)
+    msg = request.args.get('msg') or 'Tidak ada pesan'
+    print(f"Pesan diterima: {msg}")
+    return f"Pesan diterima: {msg}", 200
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
